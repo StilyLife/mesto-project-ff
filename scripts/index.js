b3d6e58287bdd82e457e00637a83b365fa7cb613
Stilylife <xetium@yandex.ru>
2024-05-23 21:22:06 +0300

    проект

--- scripts/index.js
+++ scripts/index.js
@@ -1,9 +1,28 @@
-// @todo: Темплейт карточки
+const placesContainer = document.querySelector(".places__list");
+const placeTemplate = document.querySelector("#card-template").content;
 
-// @todo: DOM узлы
+function deleteCard(event) {
+  const cardDelete = event.target.closest(".card");
+  cardDelete.remove();
+}
 
-// @todo: Функция создания карточки
+function renderCard({ name, link }, deleteCard) {
+  const placeElement = placeTemplate
+    .querySelector(".places__item")
+    .cloneNode(true);
+  placeElement.querySelector(".card__title").textContent = name;
+  placeElement.querySelector(".card__image").src = link;
+  placeElement.querySelector(".card__image").alt = name;
+  placeElement
+    .querySelector(".card__delete-button")
+    .addEventListener("click", deleteCard);
+  placesContainer.append(placeElement);
+}
 
-// @todo: Функция удаления карточки
+function render() {
+  initialCards.forEach(function (item) {
+    renderCard(item, deleteCard);
+  });
+}
 
-// @todo: Вывести карточки на страницу
+render();
