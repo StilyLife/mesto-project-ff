55c4db7721cbcf8c04a846171a4ca1493d5b25f7
Stilylife <xetium@yandex.ru>
2024-05-24 09:32:31 +0300

    проект

--- scripts/index.js
+++ scripts/index.js
@@ -1,27 +1,34 @@
 const placesContainer = document.querySelector(".places__list");
 const placeTemplate = document.querySelector("#card-template").content;
 
-function deleteCard(event) {
-  const cardDelete = event.target.closest(".card");
-  cardDelete.remove();
+function deleteCard(cardElement) {
+  cardElement.remove();
 }
 
-function renderCard({ name, link }, deleteCard) {
-  const placeElement = placeTemplate
-    .querySelector(".places__item")
-    .cloneNode(true);
-  placeElement.querySelector(".card__title").textContent = name;
-  placeElement.querySelector(".card__image").src = link;
-  placeElement.querySelector(".card__image").alt = name;
-  placeElement
-    .querySelector(".card__delete-button")
-    .addEventListener("click", deleteCard);
-  placesContainer.append(placeElement);
+function createCard({ name, link }) {
+  const cardElement = placeTemplate.querySelector(".places__item").cloneNode(true);
+
+  const cardTitle = cardElement.querySelector(".card__title");
+  cardTitle.textContent = name;
+
+  const cardImage = cardElement.querySelector(".card__image");
+  cardImage.src = link;
+  cardImage.alt = name;
+
+  const deleteButton = cardElement.querySelector(".card__delete-button");
+  deleteButton.addEventListener("click", () => deleteCard(cardElement));
+
+  return cardElement;
+}
+
+function renderCard(cardElement) {
+  placesContainer.append(cardElement);
 }
 
 function render() {
-  initialCards.forEach(function (item) {
-    renderCard(item, deleteCard);
+  initialCards.forEach(item => {
+    const cardElement = createCard(item);
+    renderCard(cardElement);
   });
 }
 
