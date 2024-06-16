e73641aaa3d2e6ef35c436898a6bed4453815e13
StilyLife <158839130+StilyLife@users.noreply.github.com>
2024-06-16 15:14:53 +0300

    Update modal.js

--- src/components/modal.js
+++ src/components/modal.js
@@ -1,32 +1,15 @@
 // Функция для открытия модального окна
 export function openPopup(popup) {
-  popup.classList.add("popup_opened");
-  popup.classList.add("popup_is-animated");
-  setTimeout(() => {
-    popup.style.visibility = "visible";
-    popup.style.opacity = "1";
-    popup.style.pointerEvents = "auto";
-    popup.style.userSelect = "auto";
-  }, 0);
-  document.addEventListener("keydown", closePopupOnEsc);
-  document.addEventListener("mousedown", closePopupOnOverlay);
+  popup.classList.add('popup_opened');
+  document.addEventListener('keydown', closePopupOnEsc);
+  document.addEventListener('mousedown', closePopupOnOverlay);
 }
 
 // Функция для закрытия модального окна
 export function closePopup(popup) {
-  popup.style.opacity = "0";
-  popup.style.pointerEvents = "none";
-  popup.style.userSelect = "none";
-  popup.addEventListener(
-    "transitionend",
-    () => {
-      popup.style.visibility = "hidden";
-      popup.classList.remove("popup_opened", "popup_is-animated");
-    },
-    { once: true }
-  );
-  document.removeEventListener("keydown", closePopupOnEsc);
-  document.removeEventListener("mousedown", closePopupOnOverlay);
+  popup.classList.remove('popup_opened');
+  document.removeEventListener('keydown', closePopupOnEsc);
+  document.removeEventListener('mousedown', closePopupOnOverlay);
 }
 
 // Функция для закрытия модального окна по нажатию на Esc
@@ -39,7 +22,7 @@ function closePopupOnEsc(evt) {
   }
 }
 
-// Функция для закрытия модального окна по клику на оверлей
+// Функция для закрытия модального окна по клику на  оверлей
 function closePopupOnOverlay(evt) {
   const openedPopup = document.querySelector(".popup_opened");
   if (openedPopup && evt.target === openedPopup) {
